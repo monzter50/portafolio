273362e2305529016cbd70f0c3c448695c9314bf
monster <josemonzter50@gmail.com>
2019-12-04 23:33:15 -0500

    Modifie webdesign headear component

---
+++ src/components/HeaderLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="hero" id="home">
+    <div class="monster container">
+      <article class="layout-about__content padding-y">
+        <vue-typed-js
+          :strings="['Front-end Developer', 'Web Designer', 'Teacher','Engineer Software']"
+          :loop="true"
+        >
+          <h1 class="margin--none font-size--md">
+            Hi, I'm a
+            <span class="text--primary">
+              <span class="typing"></span>
+            </span>
+          </h1>
+        </vue-typed-js>
+        <p>I am a creative, orderly, analytical, self-taught, passionate about teaching and learning person. My favorite hobbies is watch movies, talk with my wife, run, read articles about new update programming language and study new things.</p>
+        <p>Community Lead of the Sloth Tech looking for growth in the southeast area in Technology.</p>
+        <p>
+          Sigueme en:
+          <a href="http://" class="margin-left text--second">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
+          <a href="http://" class="margin-left text--second">
+            <font-awesome-icon :icon="['fab', 'codepen']" />
+          </a>
+          <a href="http://" class="margin-left text--second">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </a>
+          <a href="http://" class="margin-left text--second">
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+          </a>
+          <a href="http://" class="margin-left text--second">
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+        </p>
+      </article>
+      <!-- <img src="../assets/image/profile.svg" class="monster-dev" alt="Monster Developer" /> -->
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
---
+++ src/components/Portafolio.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="layout-development padding-y--lg" id="portafolio">
+    <div class="container">
+      <h2 class="title-underline">Projects</h2>
+      <div class="cards-portafolio-default">
+        <a class="card-portafolio-default" target="_blank" href="https://www.corder.band/">
+          <img src="..\assets\image\corderband.jpg" alt />
+
+          <div class="card-portafolio__description" >
+            <article class="card-content">
+              <h2 class="margin--none">Corder Band</h2>
+            </article>
+          </div>
+        </a>
+        <a class="card-portafolio-default" target="_blank" href="https://kukuarestaurantpuntacana.com/">
+          <img src="..\assets\image\kukua.jpg" alt />
+          <div class="card-portafolio__description">
+            <article class="card-content">
+              <h2 class="margin--none">Xtabay</h2>
+            </article>
+          </div>
+        </a>
+        <a class="card-portafolio-default" target="_blank" href="https://blog.on-prana.com/">
+          <img src="..\assets\image\onprana.png" alt />
+          <div class="card-portafolio__description">
+            <article class="card-content">
+              <h2 class="margin--none">Xtabay</h2>
+            </article>
+          </div>
+        </a>
+        <a class="card-portafolio-default" target="_blank" href="https://xtabaymedicinaestetica.com/">
+          <img src="..\assets\image\xtabay.jpg" alt />
+          <div class="card-portafolio__description">
+            <article class="card-content">
+              <h2 class="margin--none">Xtabay</h2>
+            </article>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "portafolio"
+};
+</script>
