b0f05db3a5d642846413a27a4274924ed7c424e5
jose gonzalez <josemonzter50@gmail.com>
2020-02-27 23:11:58 -0500

    Config CI deployment with travis

--- src/components/Portafolio.vue
+++ src/components/Portafolio.vue
@@ -4,7 +4,7 @@
       <h2 class="title-underline">Projects</h2>
       <div class="cards-portafolio-default">
         <a class="card-portafolio-default" target="_blank" href="https://www.corder.band/">
-          <img src="..\assets\image\corderband.jpg" alt />
+          <img :src="coderImg" alt />
 
           <div class="card-portafolio__description" >
             <article class="card-content">
@@ -13,15 +13,15 @@
           </div>
         </a>
         <a class="card-portafolio-default" target="_blank" href="https://kukuarestaurantpuntacana.com/">
-          <img src="..\assets\image\kukua.jpg" alt />
+          <img :src="kukuaImg" alt />
           <div class="card-portafolio__description">
             <article class="card-content">
               <h2 class="margin--none">Xtabay</h2>
             </article>
           </div>
         </a>
-        <a class="card-portafolio-default" target="_blank" href="https://blog.on-prana.com/">
-          <img src="..\assets\image\onprana.png" alt />
+        <a class="card-portafolio-default" target="_blank" href="https://on-prana.com/">
+          <img :src="onpranaImg" alt />
           <div class="card-portafolio__description">
             <article class="card-content">
               <h2 class="margin--none">Xtabay</h2>
@@ -29,7 +29,7 @@
           </div>
         </a>
         <a class="card-portafolio-default" target="_blank" href="https://xtabaymedicinaestetica.com/">
-          <img src="..\assets\image\xtabay.jpg" alt />
+          <img :src="xtabayImg" alt />
           <div class="card-portafolio__description">
             <article class="card-content">
               <h2 class="margin--none">Xtabay</h2>
@@ -42,7 +42,19 @@
 </template>
 
 <script>
+import kukuaImg from '../assets/image/kukua.jpg'
+import onpranaImg from '../assets/image/onprana.png'
+import coderImg from '../assets/image/corderband.jpg'
+import xtabayImg from '../assets/image/xtabay.jpg'
 export default {
-  name: "portafolio"
+  name: "portafolio",
+  data: function () {
+    return {
+      kukuaImg: kukuaImg,
+      onpranaImg: onpranaImg,
+      coderImg: coderImg,
+      xtabayImg:xtabayImg
+    }
+  },
 };
 </script>
